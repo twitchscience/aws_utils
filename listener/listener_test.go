e34cc0edd47deb0701bd37b4db39420bdaabdb29
Sean Lydon <lydon@justin.tv>
2017-09-15 12:13:55 -0700

    Fixing crossed wires in DedupSQSFilter
    Summary: Had True/False reversed. Made better tests, docs, and fixed.
    
    Test Plan: Unit test
    
    Reviewers: jackgao
    
    Reviewed By: jackgao
    
    Subscribers: #scieng
    
    Differential Revision: https://phabricator.internal.justin.tv/D5744

--- listener/listener_test.go
+++ listener/listener_test.go
@@ -29,34 +29,32 @@ func (h *testSQSHandler) Handle(msg *sqs.Message) error {
 
 func TestSQSListenerHandling(t *testing.T) {
 	uniq := 1000
+	runs := 3
 	handler := &testSQSHandler{Called: 0}
 	listener := BuildSQSListener(handler, time.Second, &mockSQSClient{}, nil)
-	for i := 0; i < uniq; i++ {
-		s := fmt.Sprintf("%v", i)
-		listener.handle(&sqs.Message{Body: &s}, nil)
+	for r := 0; r < runs; r++ {
+		for i := 0; i < uniq; i++ {
+			s := fmt.Sprintf("%v", i)
+			listener.handle(&sqs.Message{Body: &s}, nil)
+		}
 	}
-	for i := 0; i < uniq; i++ {
-		s := fmt.Sprintf("%v", i)
-		listener.handle(&sqs.Message{Body: &s}, nil)
-	}
-	if handler.Called != 2*uniq {
-		t.Errorf("expected %d but got %d", 2*uniq, handler.Called)
+	if handler.Called != runs*uniq {
+		t.Errorf("expected %d but got %d", runs*uniq, handler.Called)
 	}
 }
 
 func TestSQSListenerHandlingDedup(t *testing.T) {
 	logger.Init("warn")
 	uniq := 1000
+	runs := 3
 	handler := &testSQSHandler{Called: 0}
 	filter := NewDedupSQSFilter(uniq, time.Hour)
 	listener := BuildSQSListener(handler, time.Second, &mockSQSClient{}, filter)
-	for i := 0; i < uniq; i++ {
-		s := fmt.Sprintf("%v", i)
-		listener.handle(&sqs.Message{Body: &s}, nil)
-	}
-	for i := 0; i < uniq; i++ {
-		s := fmt.Sprintf("%v", i)
-		listener.handle(&sqs.Message{Body: &s}, nil)
+	for r := 0; r < runs; r++ {
+		for i := 0; i < uniq; i++ {
+			s := fmt.Sprintf("%v", i)
+			listener.handle(&sqs.Message{Body: &s}, nil)
+		}
 	}
 	if handler.Called != uniq {
 		t.Errorf("expected %d but got %d", uniq, handler.Called)
